4afefbd700d1dbd616d17877f975919e5420530b
ahjook <merrycris.ajoc@student.passerellesnumeriques.org>
2020-01-28 07:28:10 +0800

    updated version developerL&H

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -7,8 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+  onSignUp(){
+    alert('Next step is to login to make sure you can login...')
+    this.router.navigate([''])
   }
 }
