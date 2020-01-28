4d453751bf174fb10aa650f2359e8706a9c873d3
ahjook <merrycris.ajoc@student.passerellesnumeriques.org>
2020-01-28 18:55:40 +0800

    mypost kulang

--- src/app/mypost/mypost.component.ts
+++ src/app/mypost/mypost.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 
+
 const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type': 'application/json',
@@ -17,15 +18,33 @@ const httpOptions = {
 })
 export class MypostComponent implements OnInit {
 
+  yes = false
+  yesOrNo = true
+  showNotification: boolean;
+  dataArr: any[] = []
+
+
   constructor(private dataService: ApiService) {
 
   }
 
-  ngOnInit(){
+  ngOnInit() {
+    return this.dataService.shouldGetAllItem().subscribe(data => (this.dataArr = data));
   }
 
+  yesClicked() {
+    this.yesOrNo = false
+    this.yes = true
+  }
 
+  back() {
+    this.yesOrNo = true
+    this.yes = false;
+    this.showNotification = true;
 
-
+    // setTimeout(function () {
+    //   this.showNotification = true;
+    // }, 3000);
+  }
 
 }
